9680c4b90066c6b31430ef53c9666f577de7ee95
edinaraalencar <edynaraccb@gmail.com>
2021-03-02 20:57:26 -0300

    Criando funções

---
+++ teste2.py
@@ -0,0 +1,24 @@
+'''
+Faça quatro funções que retornem respectivamente
+(Nome, idade, Estado civil e Gênero)
+'''
+meu_nome = "Edinara"
+minha_idade = 22
+meu_estado_civil = "A espera de um milagre"
+meu_genero = "Feminino"
+
+def nome(meu_nome):
+    return meu_nome
+print (nome(meu_nome))
+
+def idade(minha_idade):
+    return minha_idade
+print (idade(minha_idade))
+
+def estadocivil (meu_estado_civil):
+    return meu_estado_civil
+print (estadocivil(meu_estado_civil))
+
+def genero(meu_genero):
+    return meu_genero
+print (genero(meu_genero))

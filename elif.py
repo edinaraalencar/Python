75e3f13cf834770b22abc71b3cb8bbb7b6c69434
edinaraalencar <edynaraccb@gmail.com>
2021-03-06 18:56:45 -0300

    Adicionei o fstring

---
+++ elif.py
@@ -0,0 +1,27 @@
+'''
+ = input ("Digite o primeiro numero: ")
+num2 = input ("Digite o segundo numero: ")
+op = input ("Digite um operador: ")
+resultado = None
+
+def operacao(num1, num2, op):
+    if op == "soma":
+       resultado = num1 + num2
+    elif op == "subtracao":
+        resultado = num1 - num2
+   # else:
+        #resultado = num1 * num2
+    
+    print (f" {resultado}resultado")
+
+'''
+
+'''
+for i in range (50, 101):
+    if i %  2 != 0
+        print (i)
+'''
+
+for i in range (50, 101):
+    if i %  2 != 0:
+        print (i)
--- teste2.py
+++ teste2.py
@@ -4,21 +4,21 @@ Faça quatro funções que retornem respectivamente
 '''
 meu_nome = "Edinara"
 minha_idade = 22
-meu_estado_civil = "A espera de um milagre"
-meu_genero = "Feminino"
+meu_estado_civil = "a espera de um milagre"
+meu_genero = "feminino"
 
 def nome(meu_nome):
     return meu_nome
-print (nome(meu_nome))
+print (f'Meu nome é {nome(meu_nome)}')
 
 def idade(minha_idade):
     return minha_idade
-print (idade(minha_idade))
+print (f'Minha idade é {idade(minha_idade)} anos')
 
-def estadocivil (meu_estado_civil):
+def estado_civil (meu_estado_civil):
     return meu_estado_civil
-print (estadocivil(meu_estado_civil))
+print (f'Meu estado civil se resume em {estado_civil(meu_estado_civil)}')
 
 def genero(meu_genero):
     return meu_genero
-print (genero(meu_genero))
+print (f'Meu gênero é {genero(meu_genero)}')

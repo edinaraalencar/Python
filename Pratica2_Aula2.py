51efa0b400cef922ca3865bcb4e452b85f9a750b
edinaraalencar <edynaraccb@gmail.com>
2021-03-07 13:13:51 -0300

    Passei a função estadoCivil como argumento da função match.

--- Pratica2_Aula2.py
+++ Pratica2_Aula2.py
@@ -7,18 +7,25 @@ o seu estado civil(função) e o de outra pessoa,
 e retorne "Bora" se ambos forem solteiros ou 
 "Deixa quieto" se algum for Casado
 '''
-def match(user1, user2):
-    if (user1 == 2) and (user2 == 2):
-        print("Deixa quieto")
-    elif (user1 == 1) and (user2 == 1):
-        print("Bora")
-    elif ((user1 == 1 or 2) and (user2 == 2 or 1)):
-        print("Deixa Quieto")
-    else:                         #Essa condição não está sendo satisfeita
-        print("Opção inválida !") #Quero que ela seja satisfeita usando 1 e 7
+estado_civil = None #Cria variavel e não atribui valor
 
-#Função Principal
-print("Digite 1 - Solteiro\n Digite 2 - Casado")
-user1 = int (input("Digite o Estado Civil do Primeiro Usuario: "))
-user2 = int (input("Digite o Estado Civil do Segundo Usuario: "))
-match(user1, user2)
+def estadoCivil (estado_civil): #Cria a função e passa um argumento
+    estado_civil = int (input ("Qual o estado civil-->")) #A variavel recebe o valor digitado
+    return estado_civil #Guarda o valor digitado pelo usuario
+
+#estadoCivil (estado_civil)
+
+def match(user1, user2): #A função precisa receber 2 estados civil
+    if (user1 == user2 == 1): 
+        print("Bora")
+    elif (user1 == user2 == 2):
+        print("Deixa quieto")
+    elif ((user1 == 2) and (user2 == 1)) or ((user2 == 2) and (user1 == 1)):
+        print("Deixa quieto")
+    else:
+        print("Opção Inválida!") #Condição de controle de dados fora do esperado
+
+print ("Opções:\n1 - solteiro\n2 - casado")
+match(estadoCivil(estado_civil), estadoCivil(estado_civil)) #Função que recebe outra função como parametro
+
+

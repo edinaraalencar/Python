d1e61f9d965fde6125482746432898f719985e75
edinaraalencar <edynaraccb@gmail.com>
2021-03-10 15:16:17 -0300

    Exercicios da lista1

---
+++ Lista1/1.py
@@ -0,0 +1,32 @@
+
+print("1 - Converta as seguintes expressões matemáticas\npara que possam ser calculadas usando Python: ")
+
+print("a) 10 + 20 x 30")
+print("b) 4² / 3")
+print("c) (9⁴ + 2) x 6 -1")
+print("d) (9⁴ + 2)⁴ + ( 10 / 1)\n")
+print("Respostas: \n")
+
+
+def função(a, b, c):
+    print (f'a) {a + b * c}')
+
+função(10, 20, 30)
+
+
+def função (a, b):
+    print (f'b) {(a ** 2) / b}')
+
+função(4, 3)
+
+
+def função (a, b, c, d):
+    print (f'c) {((a ** 4) + b) * (c - d)}')
+
+função(9, 2, 6, 1)
+
+
+def função (a, b, c, d):
+    print (f'd) {(((9 ** 4) + 2) ** 4) + (10 / 1)}')
+
+função(9, 2, 10, 1)

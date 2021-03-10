349b64ee6176c35848154922c3a09632d842a308
edinaraalencar <edynaraccb@gmail.com>
2021-03-10 15:39:44 -0300

    .

---
+++ Lista1/3.py
@@ -0,0 +1,9 @@
+print("3 - Crie uma função que retorne o resto da divisão do\nresultado da função da questão anterior por 2")
+
+def função(a, b):
+    return ((a * 2) * (b * 3))
+
+def função2(função):
+    return função % 2
+
+print(f'O resto da divisão é: {função2(função(1,2))}')

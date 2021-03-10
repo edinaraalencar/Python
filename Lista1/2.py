1f92ed0eb8e70155294cb3b04c4dcfce89deb7eb
edinaraalencar <edynaraccb@gmail.com>
2021-03-10 15:28:37 -0300

    .

---
+++ Lista1/2.py
@@ -0,0 +1,6 @@
+print("2 - Escreva uma função que receba dois números como\nargumento e retorne o produto do dobro do primeiro\npelo triplo do segundo")
+
+def função(a, b):
+    return ((a * 2) * (b * 3))
+
+print(função(2, 2))

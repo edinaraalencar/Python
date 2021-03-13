783f59d3c8c4b7df0fd22c121ead113a61c5f1b2
edinaraalencar <edynaraccb@gmail.com>
2021-03-13 20:38:57 -0300

    Transformação de Tempo

--- Lista1/10.py
+++ Lista1/10.py
@@ -2,32 +2,18 @@ print("""10 - Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um cert
 (8min15s por quilômetro), então 3 quilômetros a uma passo mais rápido(7min12s por quilômetro) 
 e 1 quilômetro no mesmo passo usado em primeiro lugar, que horas chego em casa para o café da manhã?\n""")
 # resultado esperado= 7:30:06
-saida_hora = 6
-saida_min = 52
-saida_seg = 0
-km = 1
+total_min = ((2*8) + (3*7)) * 60 #Minutos transformados em segundos
+total_seg = ((2*15) + (3*12)) + total_min #Total percorrido em segundos
 
-if km == 1:
-    saida_min += 8
-    saida_seg += 15
-    if saida_min >= 60:
-        saida_hora += 1
-        saida_min -= 60
-    km += 1
+saida_seg = (6*60*60) + (52*60) #Horario de saida transformado em segundos
+total = total_seg + saida_seg #Soma do horario de saida com o tempo percorrido
 
-while (5 > km > 1):
-    saida_min += 7
-    saida_seg +=12
-    if saida_min >= 60:
-        saida_hora += 1
-        saida_min -= 60
-    km += 1
+total_seg = total%60 #Armazenando o resto da divisão 
+total_min = total//60 #Armazenando o valor inteiro da divisão 
+total_horas = total_min//60 #Armazenando o valor inteiro 
+total_min = total_min%60 #Armazenando o resto da divisão 
 
-if km == 5:
-    saida_min += 8
-    saida_seg += 15
-    if saida_seg >= 60:
-        saida_min += 1
-        saida_seg -=60
 
-print(f'Horario de chegada {saida_hora}:{saida_min}:{saida_seg}')
+print(f'{total_horas}:{total_min}:{total_seg}')
+
+

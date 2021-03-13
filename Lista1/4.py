4641af8cb59803dd3fe4631053253c620878e86d
edinaraalencar <edynaraccb@gmail.com>
2021-03-13 14:20:41 -0300

    Fumante

--- Lista1/4.py
+++ Lista1/4.py
@@ -1,4 +1,7 @@
-print("4 - Escreva uma função para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro, a função deverá retornar quantos dias de vida um fumante perderá. Exiba o total em dias.\n")
+print("""4 - Escreva uma função para calcular a redução do tempo de vida de um fumante.
+Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. 
+Considere que um fumante perde 10 minutos de vida a cada cigarro, a função deverá 
+retornar quantos dias de vida um fumante perderá. Exiba o total em dias.\n""")
 
 def tempoVida(quant, anos):
     dias = anos * 365 #Transformado anos em dias

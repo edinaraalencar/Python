d7373b2dea21116d866c38ab108cf67fecf08a3d
edinaraalencar <edynaraccb@gmail.com>
2021-03-20 18:18:41 -0300

    Concatenando listas

--- Lista4/1.py
+++ Lista4/1.py
@@ -1,11 +1,11 @@
 print("""1 - Faça uma função que receba duas listas e que gere uma terceira com os 
 elementos das duas primeiras.\n""")
 
-pares = [2, 4, 6, 8, 10]
-impares = [1, 3, 5, 7, 9]
+pares = [x for x in range(0, 11, 2)]
+impares = [x for x in range (1, 10, 2)]
 
 def teste(impares, pares):
-    list = [impares, pares]
+    list = pares + impares
     print(list)
 
 print("Nova Lista criada: ")

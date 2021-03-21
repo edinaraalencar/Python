90a612c0df4d1003f80e90e19bee20702b06fee4
edinaraalencar <edynaraccb@gmail.com>
2021-03-21 19:27:04 -0300

    .

--- Lista4/5.py
+++ Lista4/5.py
@@ -5,7 +5,12 @@ a- imprima o primeiro quarto da lista
 b -imprima o último quarto da lista
 c -imprima os 5 itens do meio da lista
 d -imprima a lista reversa
-e - Imprima o primeiro e o último item da lista""")
+e - Imprima o primeiro e o último item da lista
+
+Utilize list comprehension para :
+
+f - Gerar uma lista com o quadrado de cada item da lista original
+g - Gerar uma lista com apenas itens pares da lista original""")
 
 inteiros = [x for x in range(1, 41)]
 print(inteiros)
@@ -14,4 +19,10 @@ print(inteiros[30:40])
 print(inteiros[18:23])
 print(inteiros[ : :-1])
 print(f'Primeiro item: [{inteiros[0]}], ultimo item:[{inteiros[39]}]')
+
+quadrado = [x**2 for x in range(1, 41)]
+print(quadrado)
+
+pares = [x for x in range(1, 41) if x % 2 == 0]
+print(pares)
 

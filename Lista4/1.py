bce49ee8248d38fccba228f6a1839c348d3a1259
edinaraalencar <edynaraccb@gmail.com>
2021-03-21 12:04:57 -0300

    .

--- Lista4/1.py
+++ Lista4/1.py
@@ -1,11 +1,11 @@
 print("""1 - Faça uma função que receba duas listas e que gere uma terceira com os 
 elementos das duas primeiras.\n""")
 
-pares = [x for x in range(0, 11, 2)]
-impares = [x for x in range (1, 10, 2)]
+pares = [x for x in range(0, 11, 2)] #Usando o list comprehension para gerar a lista pares
+impares = [x for x in range (1, 15, 3)] 
 
 def teste(impares, pares):
-    numeros = pares + impares
+    numeros = pares + impares #Concatenando listas
     print(numeros)
 
 print("Nova Lista criada: ")

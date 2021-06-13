a29a9cef63416fbec0594c3830ff05dfd8d6e3e5
edinaraalencar <edynaraccb@gmail.com>
2021-06-13 11:17:09 -0300

    Jogo para alfabetização

---
+++ Jogos/formar_palavras.py
@@ -0,0 +1,53 @@
+"""
+Ajudando crianças a formar palavras.
+"""
+
+texto = input('Digite uma letra: ')
+
+for letra in texto:
+    if letra == 'a':
+        print(f'{letra} de avião')
+    elif letra == 'b':
+        print(f'{letra} de bola')
+    elif letra == 'c':
+        print(f'{letra} de casa')
+    elif letra == 'd':
+        print(f'{letra} de dado')
+    elif letra == 'e':
+        print(f'{letra} de escola')
+    elif letra == 'f':
+        print(f'{letra} de feijão')
+    elif letra == 'g':
+        print(f'{letra} de gato')
+    elif letra == 'h':
+        print(f'{letra} de homem')
+    elif letra == 'i':
+        print(f'{letra} de igreja')
+    elif letra == 'j':
+        print(f'{letra} de jacare')
+    elif letra == 'l':
+        print(f'{letra} de laranja')
+    elif letra == 'm':
+        print(f'{letra} de menino')
+    elif letra == 'n':
+        print(f'{letra} de navio')
+    elif letra == 'o':
+        print(f'{letra} de ovo')
+    elif letra == 'p':
+        print(f'{letra} de pipa')
+    elif letra == 'q':
+        print(f'{letra} de queijo')
+    elif letra == 'r':
+        print(f'{letra} de rei')
+    elif letra == 's':
+        print(f'{letra} de sorvete')
+    elif letra == 't':
+        print(f'{letra} de tesoura')
+    elif letra == 'u':
+        print(f'{letra} de urso')
+    elif letra == 'v':
+        print(f'{letra} de vela')
+    elif letra == 'x':
+        print(f'{letra} de xadrez')
+    elif letra == 'z':
+        print(f'{letra} de zebra')
